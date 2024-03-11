ce82aef7e6339693a569a6afc1ad697ef8365bb7
khanatal92 <akhan126@insite.4cd.edu>
2024-03-11 13:24:00 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@ int main()
 	int n1, n2;
 	int N;
 
-	cin >> n1 >> n2;
-	cin >> N;
-
 	cout << "Enter the first initial value: ";
 	cin >> n1;
 	cout << "Enter the second initial value: ";
@@ -16,4 +13,17 @@ int main()
 
 	cout << "Enter the number of sequences (N > 2): ";
 	cin >> N;
-	
+
+	cout << n1 << " " << n2 << " ";
+
+	for (int i = 3; i <= N; i++) {
+		int sum = n1 + n2;
+		cout << sum << " ";
+		n1 = n2;
+		n2 = sum;
+	}
+
+	cout << endl;
+
+	return 0;
+}
